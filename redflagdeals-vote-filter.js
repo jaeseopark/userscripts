e92dd3af4a7c47d88558786bd84a193a7622b897
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-10-02 17:10:44 -0600

    Update redflagdeals-vote-filter.js

--- redflagdeals-vote-filter.js
+++ redflagdeals-vote-filter.js
@@ -8,21 +8,6 @@
 
 const THRESHOLD = 50;
 
-function addGlobalStyle(css) {
-    var head, style;
-    head = document.getElementsByTagName('head')[0];
-    if (!head) { return; }
-    style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = css;
-    head.appendChild(style);
-}
-
-function hideElement(selector) {
-    $(selector).css({ 'display': 'none' });
-    //addGlobalStyle(selector + " { display:none; } ");
-}
-
 $("li.topic").each(function () {
     let vote = 0;
     try {
